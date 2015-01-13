1315df699cc3332d23e4026cb648b2aa82180a14
Chisci <Tchisci@laposte.net>
2015-01-13 14:26:24 +0100

    Adding the maven project

---
+++ src/main/java/fr/uds/info901/RumorYAP/App.java
@@ -0,0 +1,13 @@
+package fr.uds.info901.RumorYAP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
